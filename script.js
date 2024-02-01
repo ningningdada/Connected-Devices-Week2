259853487da2cce1782f10f886d316e838013149
Damon Chen <hnc247@nyu.edu>
2024-01-31 20:17:54 -0500

    add files

---
+++ script.js
@@ -0,0 +1,9 @@
+//let myButton = documnet.getElementByID('myButton');
+//let myButtons = document.getElementsByClassName('myButtons');
+
+function handleClick(){
+    document.getElementByID('myButton').style.backgroundColor = "red";
+    //myButton.style.backgroundColor = "red";
+    //myButton.innerHTML = "clicked";
+    
+}
