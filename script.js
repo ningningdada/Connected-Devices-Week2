32910db271c367f6dd2f78582cd299716239e78c
Damon Chen <hnc247@nyu.edu>
2024-02-07 16:04:32 -0500

    add response input box

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-//let myButton = documnet.getElementByID('myButton');
+//let myButton = documnet.getElementById('myButton');
 //let myButtons = document.getElementsByClassName('myButtons');
 
-function handleClick(){
-    document.getElementByID('myButton').style.backgroundColor = "red";
-    //myButton.style.backgroundColor = "red";
-    //myButton.innerHTML = "clicked";
+// function handleClick(){
+//     document.getElementBsyId('myButton').style.backgroundColor = "red";
+//     //myButton.style.backgroundColor = "red";
+//     //myButton.innerHTML = "clicked";
+    
+// }
+
+const txt1 = document.getElementById('user1');
+const txt2 = document.getElementById('val1');
+const btn1 = document.getElementById('btn1');
+const out1 = document.getElementById('output1');
+const out2 = document.getElementById('output2');
+
+function fun1(){
+    out1.innerHTML = txt1.value;
+    out2.innerHTML = txt2.value;
     
 }
+
+btn1.addEventListener('click',fun1);
